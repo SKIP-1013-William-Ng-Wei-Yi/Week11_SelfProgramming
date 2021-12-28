88135d68e86651b04079042a28e4aff49b5e2ae6
William Ng Wei Yi <92504689+William284613@users.noreply.github.com>
2021-12-28 21:58:00 +0800

    Create Output array1.java

---
+++ Output array1.java
@@ -0,0 +1,78 @@
+Please Enter the 1. reverse name :  abu
+Please Enter the 2. reverse name :  ali
+Please Enter the 3. reverse name :  tan
+Please Enter the 4. reverse name :  chong
+Please Enter the 5. reverse name :  damo
+The costomer reverse name as below : 
+1. abu
+2. ali
+3. tan
+4. chong
+5. damo
+Enter Value for sales 
+1
+2
+3
+4
+5
+6
+7
+8
+9
+k = 0 y = 0
+Value of sales is 1
+k = 0 y = 1
+Value of sales is 2
+k = 0 y = 2
+Value of sales is 3
+k = 1 y = 0
+Value of sales is 4
+k = 1 y = 1
+Value of sales is 5
+k = 1 y = 2
+Value of sales is 6
+k = 2 y = 0
+Value of sales is 7
+k = 2 y = 1
+Value of sales is 8
+k = 2 y = 2
+Value of sales is 9
+Enter index No.0
+1
+Enter index No.1
+2
+Enter index No.2
+3
+Enter index No.3
+4
+Enter index No.4
+5
+Item Idex No. 0 are : 1
+Item Idex No. 1 are : 2
+Item Idex No. 2 are : 3
+Item Idex No. 3 are : 4
+Item Idex No. 4 are : 5
+Sum of the item : 15
+Average of the item : 3
+
+Enter Five customer name :
+abu
+tan
+chong
+damo
+sam
+Customer name reverse order : 
+1. abu
+2. tan
+3. chong
+4. damo
+5. sam
+Enter five integers one number per line
+1
+2
+3
+4
+5
+6The sum of the numbers = 15
+The numbers in reverse order are: 
+5 4 3 2 1
