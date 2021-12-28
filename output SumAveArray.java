0299672c2da60ff644670d89e4abe72ae49c5558
William Ng Wei Yi <92504689+William284613@users.noreply.github.com>
2021-12-28 22:03:22 +0800

    Create output SumAveArray.java

---
+++ output SumAveArray.java
@@ -0,0 +1,12 @@
+Enter the 1. value :1
+Enter the 2. value :2
+Enter the 3. value :3
+Enter the 4. value :4
+Enter the 5. value :5
+Enter the 6. value :5
+Enter the 7. value :5
+Enter the 8. value :5
+Enter the 9. value :4
+Enter the 10. value :4
+Sum of the index value : 38.0
+Average of the index value : 3.8
