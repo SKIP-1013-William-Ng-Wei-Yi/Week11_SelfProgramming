7a4cef948f19596c4e8fab589fca1f720b5529e0
William Ng Wei Yi <92504689+William284613@users.noreply.github.com>
2021-12-28 21:59:37 +0800

    Create output dynamicArray.java

---
+++ output dynamicArray.java
@@ -0,0 +1,12 @@
+Enter the size of the array : 5
+Please enter the 1. int value : 1
+Please enter the 2. int value : 2
+Please enter the 3. int value : 3
+Please enter the 4. int value : 4
+Please enter the 5. int value : 5
+The value data you enter as below
+1. 1
+2. 2
+3. 3
+4. 4
+5. 5
